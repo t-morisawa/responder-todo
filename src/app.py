feb8c1ee26c607cab2821761bbc5b4bc5d865477
Toma <13.35.104@gmail.com>
2020-08-22 17:19:29 +0900

    update to add add_todo

--- src/app.py
+++ src/app.py
@@ -14,10 +14,19 @@ def test_random(req, resp):
 @api.route("/test")
 async def get_html(req, resp):
     media = {}
-    if req.method == "post":
-        media = await req.media()
-    print(media)
     resp.html = api.template('test.html', params=media)
+
+@api.route("/todo")
+async def add_todo(req, resp):
+    media = await req.media()
+    print(media)
+    # 追加処理をここに実装する
+    api.redirect(resp, '/test')
+
+@api.route("/todolist")
+async def update_todolist(req, resp):
+    media = await req.media()
+
 
 if __name__ == '__main__':
     api.run(address='0.0.0.0', port=80)
