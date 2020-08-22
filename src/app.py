d40f7f8d054f97b506df4a75066cbb1de9386dbd
hikarukoizumi <hikaru.k.ac@gmail.com>
2020-08-22 14:56:36 +0900

    no message

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 import responder
+import random
 
 api = responder.API()
 
 @api.route("/")
 def hello_world(req, resp):
     resp.text = "hello, world!"
+
+@api.route("/random")
+def test_random(req, resp):
+    resp.text = random.random()
 
 if __name__ == '__main__':
     api.run(address='0.0.0.0', port=80)
