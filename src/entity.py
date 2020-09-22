a885cd2ea6297699a3ce33e0632c615620113a29
Toma <13.35.104@gmail.com>
2020-09-22 12:21:23 +0900

    update to use usecase

--- src/entity.py
+++ src/entity.py
@@ -16,7 +16,7 @@ class Todolist:
   """
   Todoリスト本体
   """
-  todolist: List[Todo]
+  data: List[Todo]
 
 
 @dataclass
@@ -24,25 +24,4 @@ class Checklist:
   """
   Todoリストの更新に利用. ユーザがチェックした要素の配列
   """
-  checklist: List[int]
-
-
-@dataclass
-class TodolistRepository:
-  async def get_all(self):
-    """
-    Todolistの一覧を取得する
-    """
-    pass
-
-  async def add_item(self, task):
-    """
-    Todolistのアイテムを一件追加する
-    """
-    pass
-
-  async def update_checked_from_checklist(self, checklist):
-    """
-    Todolistのチェック状態を一括更新する
-    """
-    pass
+  data: List[int]
