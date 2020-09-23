068822525aca037962f557e3b9bfd12e5160a0ea
Toma <13.35.104@gmail.com>
2020-09-23 19:56:19 +0900

    update Route

--- src/app.py
+++ src/app.py
@@ -16,22 +16,22 @@ def hello_world(req, resp):
     resp.text = "hello, world!"
 
 class TodoRoute:
+    def __init__(self):
+        self.controller = TodoController(UsecaseImpl(TodolistRepositoryImpl(TodolistDriver)))
+
     async def on_get(self, req, resp):
-        controller = TodoController(UsecaseImpl(TodolistRepositoryImpl(TodolistDriver)))
-        todolist = await controller.get_all()
+        todolist = await self.controller.get_all()
         resp.html = api.template('todo.html', todolist_presenter=todolist.data)
 
     async def on_post(self, req, resp):
         media = await req.media()
         if media.get('action') == 'add_todo':
-            controller = TodoController(UsecaseImpl(TodolistRepositoryImpl(TodolistDriver)))
-            todolist = await controller.add_item(media)
+            todolist = await self.controller.add_item(media)
             resp.html = api.template('todo.html', todolist_presenter=todolist.data)
         elif media.get('action') == 'update_checklist':
             querydict = media.get_list('riyu')
             checklist = Checklist(list(map(lambda i: int(i)-1, querydict)))
-            controller = TodoController(UsecaseImpl(TodolistRepositoryImpl(TodolistDriver)))
-            todolist = await controller.update_all_from_checklist(checklist)
+            todolist = await self.controller.update_all_from_checklist(checklist)
             resp.html = api.template('todo.html', todolist_presenter=todolist.data)
 
 api.add_route('/todo', TodoRoute)
