8635e479b4919c5201166b4eb711ffcf91503634
Toma <13.35.104@gmail.com>
2020-09-23 11:32:49 +0900

    rename

--- src/app.py
+++ src/app.py
@@ -14,30 +14,30 @@ async def setup():
 def hello_world(req, resp):
     resp.text = "hello, world!"
 
-class TodoController:
+class TodoRoute:
     async def on_get(self, req, resp):
         usecase = UsecaseImpl(TodolistRepositoryImpl(TodolistDriver))
         todolist = await usecase.get_all()
-        resp.html = api.template('test.html', todolist_presenter=todolist.data)
+        resp.html = api.template('todo.html', todolist_presenter=todolist.data)
 
     async def on_post(self, req, resp):
         media = await req.media()
         if media.get('action') == 'add_todo':
             if media.get('task') is None:
-                api.redirect(resp, '/test')
+                api.redirect(resp, '/todo')
             usecase = UsecaseImpl(TodolistRepositoryImpl(TodolistDriver))
             await usecase.add_item(task=media.get('task'))
             todolist = await usecase.get_all()
-            resp.html = api.template('test.html', todolist_presenter=todolist.data)
+            resp.html = api.template('todo.html', todolist_presenter=todolist.data)
         elif media.get('action') == 'update_checklist':
             media = await req.media()
             checklist = checklist_from_form(media)
             usecase = UsecaseImpl(TodolistRepositoryImpl(TodolistDriver))
             await usecase.update_all_from_checklist(checklist)
             todolist = await usecase.get_all()
-            resp.html = api.template('test.html', todolist_presenter=todolist.data)
+            resp.html = api.template('todo.html', todolist_presenter=todolist.data)
 
-api.add_route('/todo', TodoController)
+api.add_route('/todo', TodoRoute)
 
 if __name__ == '__main__':
     api.run(address='0.0.0.0', port=80)
