0839e44386d465285c37304bca040a7ce02fe9fc
Toma <13.35.104@gmail.com>
2020-09-26 12:28:28 +0900

    update app & controller

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 import responder
-from db import init as init_db, Todolist as TodolistDriver
-from repository import TodolistRepositoryImpl
-from usecase import UsecaseImpl
+from db import init as init_db
 from controller import TodoController
 
 api = responder.API()
@@ -21,19 +19,16 @@ def hello_world(req, resp):
     resp.text = "hello, world!"
 
 class TodoRoute:
-    def __init__(self):
-        self.controller = TodoController(UsecaseImpl(TodolistRepositoryImpl(TodolistDriver)))
-
     async def on_get(self, req, resp):
-        todolist = await self.controller.get_all()
+        todolist = await TodoController().get_all()
         resp.html = api.template('todo.html', todolist_presenter=todolist.data)
 
     async def on_post(self, req, resp):
         media = await req.media()
         if media.get('action') == FORM_VALUE['ADD_TODO']:
-            todolist = await self.controller.add_item(media)
+            todolist = await TodoController().add_item(media)
         elif media.get('action') == FORM_VALUE['UPDATE_CHECKLIST']:
-            todolist = await self.controller.update_all_from_checklist(media.get_list(FORM_VALUE['CHECKLIST']))
+            todolist = await TodoController().update_all_from_checklist(media.get_list(FORM_VALUE['CHECKLIST']))
         resp.html = api.template('todo.html', todolist_presenter=todolist.data)
 
 api.add_route('/', TodoRoute)
--- src/controller.py
+++ src/controller.py
@@ -1,10 +1,12 @@
-from db import Todolist as TodolistDriver
-from repository import TodolistRepositoryImpl
 from usecase import Checklist
+from usecase import UsecaseImpl
+from repository import TodolistRepositoryImpl
+from db import Todolist as TodolistDriver
+
 
 class TodoController:
-    def __init__(self, usecase):
-        self.usecase = usecase
+    def __init__(self):
+        self.usecase = UsecaseImpl(TodolistRepositoryImpl(TodolistDriver))
 
     async def get_all(self):
         return await self.usecase.get_all()
